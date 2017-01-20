03ab5a858bb10c09d0be8cfa96801f094a85af7e
Brent Waldrop <brent@Brents-MacBook-Pro.local>
2017-01-19 21:49:12 -0600

    more progress....

--- src/components/searcBar.jsx
+++ src/components/searcBar.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-
-const SearchBar = () => {
+//Functional Component
+/*const SearchBar = () => {
     return <input />;
-};
+};*/
+
+//Class based Component
+class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {term: ''};
+    }
+    render() {
+        return (
+            <div>
+                <input 
+                    value={this.state.term}
+                    onChange={this.onInputChange}  />
+            </div>
+        );
+    }
+
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value});
+    }
+}
+
+
 
 export default SearchBar;
