ea9935237a5e9bff31670dda456a257cbea933cd
Brent Waldrop <brent@Brents-MacBook-Pro.local>
2017-01-22 22:15:41 -0600

    redux

---
+++ src/reducers/reducerBooks.jsx
@@ -0,0 +1,9 @@
+export default () => {
+    return [
+        { title: 'Javascript: the good parts' },
+        { title: 'Harry Potter' },
+        { title: 'The Dark Tower' },
+        { title: 'Javascript Patterns' }
+
+    ];
+}
