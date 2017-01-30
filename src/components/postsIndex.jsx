b1520c2db3eeface2a1ee2f0185286bd9cffac96
Brent <general@diaryofacoder.com>
2017-01-30 12:48:52 -0600

    Changed it class based component

--- src/components/postsIndex.jsx
+++ src/components/postsIndex.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
+class PostIndex extends Component {
+    componentWillMount() {
+        console.log('this would be a good time to call an action creator to fetch a post')
+    }
 
-export default () => {
-    return <div>List of blog posts.</div>
-};
+    render() {
+        return (
+            <div>List of blog posts</div>
+        )
+    }
+}
+
+export default PostIndex;
