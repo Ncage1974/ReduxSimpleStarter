70f529518410dd44a2d9f9180b30ebcfdc3b55c7
Brent <general@diaryofacoder.com>
2017-01-30 13:21:04 -0600

    boilerplate for new post page

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 
 import App from './components/app.jsx';
 import PostIndex from './components/postsIndex.jsx';
+import PostNew from './components/postNew.jsx';
 
 export default (
     <Route path="/" component={App} >
         <IndexRoute component={PostIndex} />
+        <Route path="posts/new" component={PostNew} />
     </Route>
 
 );
